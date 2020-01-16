faebd4e2f3b5405b4b29eede71b2aab097a4d086
Greg Stephen <GregAStephen@gmail.com>
2020-01-16 00:57:21 -0600

    logs user in and gets some info

--- discdig1/src/Helpers/Data/userRequests.js
+++ discdig1/src/Helpers/Data/userRequests.js
@@ -2,10 +2,16 @@ import axios from 'axios';
 
 const baseUrl = 'https://localhost:44370/api/user';
 
+const getUserByFirebaseUid = uid => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/uid/${uid}`)
+    .then(results => resolve(results.data))
+    .catch(err => reject(err));
+});
+
 const addNewUser = userobj => new Promise((resolve, reject) => {
   axios.post(`${baseUrl}`, userobj)
     .then(results => resolve(results.data))
     .catch(err => reject(err))
 });
 
-export default { addNewUser };
+export default { getUserByFirebaseUid, addNewUser };
