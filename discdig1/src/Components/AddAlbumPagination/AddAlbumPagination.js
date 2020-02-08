453aa907040a5d41e9959d40f8f1ff126161930e
Greg Stephen <GregAStephen@gmail.com>
2020-02-08 10:06:00 -0600

    Long ugly code but it works

--- discdig1/src/Components/AddAlbumPagination/AddAlbumPagination.js
+++ discdig1/src/Components/AddAlbumPagination/AddAlbumPagination.js
@@ -6,8 +6,70 @@ class AddAlbumPagination extends React.Component {
 
   handleClick(e, page) {
     e.preventDefault();
-    const {changePage} = this.props;
+    const { changePage } = this.props;
     changePage(page);
+  }
+
+  createLargerPagination = () => {
+    const { currentPage, totalPages } = this.props;
+    let arrayOfPages = [...Array(10)];
+    let pagesToReturn;
+    if (currentPage < 6) {
+      pagesToReturn = arrayOfPages.map((page, i) =>
+        <PaginationItem active={i + 1 === currentPage} key={i + 1}>
+          <PaginationLink onClick={e => this.handleClick(e, i + 1)} href="#">
+            {i + 1}
+          </PaginationLink>
+        </PaginationItem>
+      )
+      pagesToReturn.push(
+        <PaginationItem disabled key='...end'>
+          <PaginationLink href="#">
+            ...
+        </PaginationLink>
+        </PaginationItem>
+      )
+    }
+    else if (currentPage >= 6 && currentPage < (totalPages - 5)) {
+      pagesToReturn = arrayOfPages.map((page, i) =>
+        <PaginationItem active={(i + 1 + (currentPage - 5)) === currentPage} key={(i + 1 + (currentPage - 5))}>
+          <PaginationLink onClick={e => this.handleClick(e, (i + 1 + (currentPage - 5)))} href="#">
+            {(i + 1 + (currentPage - 5))}
+          </PaginationLink>
+        </PaginationItem>
+      )
+      pagesToReturn.unshift(
+        <PaginationItem disabled key='...beginning'>
+          <PaginationLink href="#">
+            ...
+      </PaginationLink>
+        </PaginationItem>
+      )
+      pagesToReturn.push(
+        <PaginationItem disabled key='...end'>
+          <PaginationLink href="#">
+            ...
+      </PaginationLink>
+        </PaginationItem>
+      )
+    }
+    else if (currentPage >= (totalPages - 5)) {
+      pagesToReturn = arrayOfPages.map((page, i) =>
+        <PaginationItem active={(i + 1 + (totalPages - 10)) === currentPage} key={(i + 1 + (totalPages - 10))}>
+          <PaginationLink onClick={e => this.handleClick(e, (i + 1 + (totalPages - 10)))} href="#">
+            {(i + 1 + (totalPages - 10))}
+          </PaginationLink>
+        </PaginationItem>
+      )
+      pagesToReturn.unshift(
+        <PaginationItem disabled key='...beginning'>
+          <PaginationLink href="#">
+            ...
+      </PaginationLink>
+        </PaginationItem>
+      )
+    }
+    return pagesToReturn;
   }
 
   render() {
@@ -16,9 +78,9 @@ class AddAlbumPagination extends React.Component {
     return (
       <div className="AddAlbumPagination col">
         <Pagination aria-label="Page navigation">
-        <PaginationItem disabled={currentPage <= 1}>
+          <PaginationItem disabled={currentPage <= 1}>
             <PaginationLink
-              onClick={e => this.handleClick(e, 1) }
+              onClick={e => this.handleClick(e, 1)}
               first
               href="#"
             />
@@ -31,22 +93,18 @@ class AddAlbumPagination extends React.Component {
             />
           </PaginationItem>
 
-        { arrayOfPages.length > 10 ?
-        // this is when it needs to have the ... on the end that has it
-        arrayOfPages.map((page, i) =>
-            <PaginationItem active={i + 1 === currentPage} key={i + 1}>
-              <PaginationLink onClick={e => this.handleClick(e, i + 1)} href="#">
-                {i + 1}
-              </PaginationLink>
-            </PaginationItem>
-          ) :
-          arrayOfPages.map((page, i) =>
-          <PaginationItem active={i + 1 === currentPage} key={i + 1}>
-            <PaginationLink onClick={e => this.handleClick(e, i + 1)} href="#">
-              {i + 1}
-            </PaginationLink>
-          </PaginationItem>
-        )}
+          {arrayOfPages.length > 10 ?
+
+            // this is when it needs to have the ... on the end that has it
+            this.createLargerPagination()
+            :
+            arrayOfPages.map((page, i) =>
+              <PaginationItem active={i + 1 === currentPage} key={i + 1}>
+                <PaginationLink onClick={e => this.handleClick(e, i + 1)} href="#">
+                  {i + 1}
+                </PaginationLink>
+              </PaginationItem>
+            )}
 
           <PaginationItem disabled={currentPage >= totalPages}>
             <PaginationLink
@@ -57,7 +115,7 @@ class AddAlbumPagination extends React.Component {
           </PaginationItem>
           <PaginationItem disabled={currentPage >= totalPages}>
             <PaginationLink
-              onClick={e => this.handleClick(e, totalPages) }
+              onClick={e => this.handleClick(e, totalPages)}
               last
               href="#"
             />
